be9dc8f10bdb5792c1907085cf454015bf0342e9
Anurag <aojha@skillgigs.com>
2023-11-21 00:25:38 +0530

    connect to database

--- mern-realState-project/Api/index.js
+++ mern-realState-project/Api/index.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const databaseName = process.env.DB_NAME;
+const clusterName = process.env.DB_CLUSTER_NAME;
+
+mongoose.connect(`mongodb+srv://${username}:${password}@${clusterName}/${databaseName}?retryWrites=true&w=majority`).then(() => {
+  console.log('Mr. Anurag , Connected to MongoDB!');
+})
+.catch((err) => {
+  console.log(err);
+});;
 
 const app = express();
+const port = 3000;
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
-  });
-
+app.listen(port, () => {
+  console.log(`Mr. Anurag , Server is running on port ${port}!`);
+});
