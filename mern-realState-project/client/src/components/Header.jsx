db9f3713d07f25e72697941164a90ad3cfdd1c51
Anurag <aojha@skillgigs.com>
2023-11-21 06:48:35 +0530

    complete sign up page with functionality

--- mern-realState-project/client/src/components/Header.jsx
+++ mern-realState-project/client/src/components/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
        <ul className='flex gap-4'>
        <Link to='/'> <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>  </Link>
        <Link to='About'> <li className='hidden sm:inline text-slate-700 hover:underline'>About </li> </Link>
-       <Link to='SignIn'> <li className=' text-slate-700 hover:underline'>Sign in</li> </Link>
+       <Link to='SignUp'> <li className=' text-slate-700 hover:underline'>Sign in</li> </Link>
        </ul>
        </div>
     </header>
